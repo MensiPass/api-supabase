4958abda919f04485485ea19a1d43fc00e73ce50
unknown <mensipass@gmail.com>
2026-08-26 13:00:20 +0200

    Stage 1: signup and login routes working

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
 from supabase import create_client
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse
 
 load_dotenv()
 
@@ -17,6 +19,16 @@ supabase = create_client(
 )
 
 app = FastAPI()
+
+#sign up body model
+class SignupReq(BaseModel):
+    email: str | None = None
+    password: str | None = None
+class LoginRequest(BaseModel):
+    email: str | None = None
+    password: str | None = None
+#--------------------------------------
+#Stage 0 ----------------
 #check supabase connection
 @app.on_event("startup")
 def startup():
@@ -31,3 +43,51 @@ def startup():
 @app.get("/")
 def root():
     return {"message": "API is running"}
+
+#Stage 1 ---------------------------
+#sign up call
+@app.post("/auth/signup",description="Created", status_code=201)
+def sign_up(req: SignupReq):
+    if not req.email or not req.password:
+         raise HTTPException(
+            status_code=400,
+            detail="Email and password are required"
+        )
+    try:
+        res = supabase.auth.sign_up({
+            "email": req.email,
+            "password": req.password
+        })
+
+        return res.user
+
+    except Exception as e:
+        print(f"Supabase signup error: {e}")
+        raise HTTPException(
+            status_code=400,
+            detail=str(e)
+        )
+#log in call
+@app.post("/auth/login",description="Logged in", status_code=200)
+def sign_up(req: LoginRequest):
+    if not req.email or not req.password:
+         raise HTTPException(
+            status_code=400,
+            content={"error": "Email and password are required"}
+        )
+    try:
+        res = supabase.auth.sign_in_with_password({
+            "email": req.email,
+            "password": req.password
+        })
+
+        return {
+            "access_token": res.session.access_token,
+            "refresh_token": res.session.refresh_token
+        }
+
+    except Exception:
+        return JSONResponse(
+            status_code=401,
+            content={"error": "Invalid login credentials"}
+        )
